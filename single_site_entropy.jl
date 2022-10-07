84d77f30c7219bbc0ead26015ae8ee6b471edbe6
rmelendrez <rm18n@my.fsu.edu>
2022-10-07 15:11:11 -0400

    real number for entanglement entropy

--- single_site_entropy.jl
+++ single_site_entropy.jl
@@ -9,15 +9,9 @@ function entropy_von_neumann(psi::MPS, b::Int)
   S,_ = eigen(rho)
   SvN = 0.0
   for n in 1:dim(S, 1)
-    p = S[n,n]
+    p = real(S[n,n])
     SvN -= p * log(p)
   end
   return SvN
 end
 
-N = 4
-s = siteinds("S=1/2", N)
-psi = randomMPS(s, 4)
-b = 2
-SvN = entropy_von_neumann(psi, b)
-@show SvN
